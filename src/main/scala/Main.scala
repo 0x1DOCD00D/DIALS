9560628a9423ed33f4bd4c34a510db700f59c8a8
Mark Grechanik <markgrechanik@gmail.com>
2025-08-02 11:15:46 -0500

    added the entry to the main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,13 @@
 package com.lsc
 
+import Utilz.{ConfigDb, CreateLogger}
+import org.slf4j.Logger
+
 object Main:
+  val logger: Logger = CreateLogger(classOf[ConfigDb.type])
   @main def runMain(params: String*): Unit =
-    println("Howdy, yall!")
+    logger.info(s"Running DIALS ${if params.isEmpty then "without parameters" else "with parameters: $params"}")
+    logger.info("Configuration settings:")
+    ConfigDb.apply().foreach { case (key, value) =>
+      logger.info(s"$key = $value")
+    }
